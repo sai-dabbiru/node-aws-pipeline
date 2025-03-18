bbf5bf87a342fab3a9684182c7132585b81af530
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-03-18 15:10:06 +0530

    change done from local

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express2' });
+  res.render('index', { title: 'Welcome to capstone project' });
 });
 
 module.exports = router;
