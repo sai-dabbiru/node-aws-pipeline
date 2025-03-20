3f66af45735a4c3893afeec4ac028392aade598c
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-03-20 14:18:46 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to DevOps Academy' });
 });
 
 module.exports = router;
