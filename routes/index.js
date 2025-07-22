9054f4358439239cc31a90a155b537901d518236
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-07-22 12:02:04 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express3-devops' });
+  res.render('index', { title: 'hcls-session' });
 });
 
 module.exports = router;
