d32dd6f3f6e10c1e93308de1e2ca724769e74958
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-07-23 11:18:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'updated-pipeline' });
+  res.render('index', { title: 'code pipeline demo' });
 });
 
 module.exports = router;
